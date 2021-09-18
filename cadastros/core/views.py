7b44bb116290e226a3b309476cc9450cbfa816dc
wartrax13 <pedro.moisesandrade@gmail.com>
2021-09-18 10:31:45 -0300

    Autenticacao e class Deletar

--- cadastros/core/views.py
+++ cadastros/core/views.py
@@ -1,14 +1,19 @@
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from cadastros.core.forms import CadastroForm
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from .models import Familias
 from .forms import CadastroForm
 from django.views.generic import TemplateView, ListView, UpdateView, DetailView
 
+
 def inicio(request):
     return render(request, 'index.html')
 
-
+@login_required
 def cadastro_lista(request):
     template_name = 'lista.html'
     objects = Familias.objects.all()
@@ -19,24 +24,34 @@ def cadastro_lista(request):
     return render(request, template_name, context)
 
 
+@login_required
 def cadastro_detalhe(request, pk):
     template_name = 'cadastro_detalhe.html'
     obj = Familias.objects.get(pk=pk)
     context = {'object': obj}
     return render(request, template_name, context)
 
-
+@login_required
 def cadastro_add(request):
     template_name = 'cadastro_form.html'
     return render(request, template_name)
 
 
-class CriarCadastro(CreateView):
+class CriarCadastro(LoginRequiredMixin, CreateView):
     model = Familias
     template_name = 'cadastro_form.html'
     form_class = CadastroForm
+    success_url = reverse_lazy('inicio')
+
 
 class AtualizarCadastro(UpdateView):
     model = Familias
     template_name = 'cadastro_form.html'
     form_class = CadastroForm
+    #success_url = reverse_lazy('core:inicio')
+
+class DeletarCadastro(DeleteView):
+    model = Familias
+    template_name = 'deletar_form.html'
+    form_class = CadastroForm
+    success_url = reverse_lazy('core:inicio')
